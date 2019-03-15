e4fcfab411a8bd5f334a0f16423289c276de6d5a
yael <Welowid@gmail.com>
2019-03-15 09:21:06 -0300

    junit 5 added to dependencies

---
+++ src/test/java/com/prueba/maven/ApiTest.java
@@ -0,0 +1,42 @@
+package com.prueba.maven;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+
+public class ApiTest {
+	
+	// El método @BeforeAll debe ser estático, si no, el codigo no compilará
+	@BeforeAll
+	static void setup() {
+	    System.out.println("@BeforeAll - executes once before all test methods in this class");
+	}
+	
+	@BeforeEach
+	void init() {
+	    System.out.println("@BeforeEach - executes before each test method in this class");
+	}	 
+	
+	@DisplayName("Single test successful")
+	@Test
+	void testSingleSuccessTest() {
+	    assertEquals("hola", "hola");
+	}
+
+	@DisplayName("Single test failed")
+	@Test
+	void testSingleFailTest() {
+	    assertEquals("hola", "hla");
+	}
+	
+	@AfterEach
+	void tearDown() {
+	    System.out.println("@AfterEach - executed after each test method.");
+	}
+	 
+	@AfterAll
+	static void done() {
+	    System.out.println("@AfterAll - executed after all test methods.");
+	}
+}
